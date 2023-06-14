c9c6f60687dec1e38d86746fede4512a7e68203b
LiNeoXY <lneo_a_9527@163.com>
2023-06-15 00:32:32 +0800

    web服务实现

---
+++ MyTomcat/src/com/SocketTest/CommonClass/Request.java
@@ -0,0 +1,60 @@
+package com.SocketTest.CommonClass;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Request {
+
+    //请求协议
+    private String protocol;
+    // 请求方式
+    private String method;
+    // uri
+    private String uri;
+    // 请求头
+    private Map<String, String> header = new HashMap<>();
+    // 请求体
+    private String body;
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public String getHeader(String key) {
+        return this.header.get(key);
+    }
+
+    public void setHeader(String key, String value) {
+        this.header.put(key, value);
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+
+}
---
+++ MyTomcat/src/com/SocketTest/CommonClass/RequestHandler.java
@@ -0,0 +1,12 @@
+package com.SocketTest.CommonClass;
+
+import java.io.InputStream;
+
+public class RequestHandler {
+    public static Request handle(InputStream requestStream) {
+        Request request = new Request();
+
+
+        return request;
+    }
+}
